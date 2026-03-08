b50c13428c5417dcc2c4602fa7e14177064f4a4c
francisbagsic12 <hugobayani@gmail.com>
2026-03-09 06:20:10 +0800

    Update allowed origins for CORS configuration

--- src/server.js
+++ src/server.js
@@ -23,7 +23,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:5173",
   "http://127.0.0.1:5173",
-  "sciequest.netlify.app",
+  "https://sciquest2.netlify.app",
   "https://sciquest2.netlify.app/login"
 ];
 
